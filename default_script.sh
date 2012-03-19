b57d64321e2cd0cce2c8ae1c9f994c8d4d34b3d9
old486whizz <old486whizz@gmail.com>
2012-03-19 16:01:13 +0000

    Added default_script template

---
+++ default_script.sh
@@ -0,0 +1,83 @@
+#! /bin/ksh
+
+#================================================================
+#  <SCRIPT> does the following:
+#     * LIST
+#   it does NOT:
+#     * LIST
+#   Assumptions:
+#     * LIST
+#================================================================
+# Version: x.y       DD/MM/20YY       Paul "1" Sanders
+#              Basic stuff. I rule,
+#              Initial Version Details
+# Don't forget to update "VERSION" variable below
+# ---
+#              FOR INTERNAL USE!!
+# 
+#================================================================
+
+#================================================================
+# Syntax:
+#
+#   ./<SCRIPT> [-option] <STUFF>
+#
+#  options:         .
+#    o              explain option
+#
+#  eg.
+#    ./<SCRIPT> -o stuff
+#
+#================================================================
+
+
+#================================================================
+# DEFINITION AREA
+#    (basic/global variables + functions)
+#================================================================
+alias echo="echo -e"
+DATESTAMP="`date +%Y%m%d_%H%M%S`"
+VERSION="x.y"
+SCRIPTHOME="/location"
+LOGDIR="${SCRIPTHOME}/logs"
+
+usage () {
+  echo "V.${VERSION}"
+  echo "usage: <SCRIPT> -o <STUFF>\n"
+  echo "options:"
+  echo "  -o            | explain option"
+  echo ""
+  exit 12
+}
+
+
+#================================================================
+# Simple validation of parameters:
+#================================================================
+
+INDEX=1
+set -A argsarr -- $@
+
+while [[ ${OPTIND} -le $# ]]; do
+  getopts ":o:" FLAG
+  # first : silences unknown flag errors (handled by '?' instead)
+  # : after letter indicates parameter value passed after letter
+  case ${FLAG} in
+    o) OPTION=${OPTARG}
+      ;;
+    ?) [[ -z ${OPTARG} ]] && value[INDEX]=${argsarr[(OPTIND-1)]} && ((INDEX+=1))
+       [[ -n ${OPTARG} ]] && echo "** option: '${OPTARG}' not recognized - ignoring. **" && continue
+       ((OPTIND+=1))
+      ;;
+    :) echo "** option: ${OPTARG} needs an argument. **"
+       usage
+      ;;
+  esac
+done
+
+((INDEX-=1))
+
+#================================================================
+# Main Script:
+#================================================================
+# use value[#] to access the parameters passed to the script
